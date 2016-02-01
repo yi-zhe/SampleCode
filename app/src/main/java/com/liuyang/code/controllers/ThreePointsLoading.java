6b3b9166ec9e68f9b191665b40246548fd041636
liuyang <liuyang@xiaolinxiaoli.com>
2016-02-01 10:35:01 +0800

    Three points loading

---
+++ app/src/main/java/com/liuyang/code/controllers/ThreePointsLoading.java
@@ -0,0 +1,23 @@
+package com.liuyang.code.controllers;
+
+import com.liuyang.code.R;
+
+/**
+ * @author Liuyang 2016/1/31.
+ */
+public class ThreePointsLoading extends BaseFragment {
+    @Override
+    protected int layoutId() {
+        return R.layout.three_points_loading;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void refresh() {
+
+    }
+}
