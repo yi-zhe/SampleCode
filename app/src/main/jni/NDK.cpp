d1e4eedc104f843c1a3a8ec6c11296b703e27a02
liuyang <liuyang@xiaolinxiaoli.com>
2016-02-02 18:55:39 +0800

    use cpp and use log in jni

--- app/src/main/jni/NDK.cpp
+++ app/src/main/jni/NDK.cpp
@@ -1,10 +1,13 @@
 //
 // Created by Liuyang on 2016/1/29.
 //
-
+#include <android/log.h>
 #include "com_liuyang_code_controllers_NDK.h"
+
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "TAG", __VA_ARGS__)
 
 jstring
 Java_com_liuyang_code_controllers_NDK_stringFromC(JNIEnv *env, jobject obj) {
-    return (*(*env)).NewStringUTF(env, "hello world");
+    LOGE("Hello Log");
+    return env->NewStringUTF("hello world");
 }
