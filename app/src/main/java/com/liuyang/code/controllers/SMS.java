a7f5ee5cbb84e096af103f8e0c38084900a5cd66
liuyang <liuyang@xiaolinxiaoli.com>
2016-02-03 14:48:09 +0800

    receive sms

--- app/src/main/java/com/liuyang/code/controllers/SMS.java
+++ app/src/main/java/com/liuyang/code/controllers/SMS.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
 import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
 
 import com.liuyang.code.R;
 
@@ -20,8 +21,10 @@ public class SMS extends BaseFragment {
 
     private static final String SEND_SMS_ACTION = "com.liuyang.code.controller.SMS_SEND";
     private static final String DELIVERED_SMS_ACTION = "com.liuyang.code.controller.SMS_DELIVERED";
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
     private SentReceiver sentReceiver;
     private DeliveredReceiver deliveredReceiver;
+    private ReceiveSMS receiveSMS;
 
     @Override
     protected int layoutId() {
@@ -32,7 +35,7 @@ public class SMS extends BaseFragment {
     protected void init() {
         find(R.id.send_sms_via_other_app).setOnClickListener(l -> {
             // send sms
-            Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("sms:18888888888"));
+            Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("sms:88888888888"));
             intent.putExtra("sms_body", "Press send to send me");
             startActivity(intent);
         });
@@ -42,7 +45,7 @@ public class SMS extends BaseFragment {
             Intent intent = new Intent(Intent.ACTION_SEND);
             intent.setType("image/jpeg");
             intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File("/sdcard/1.jpg")));
-            intent.putExtra("address", "188888888");
+            intent.putExtra("address", "88888888888");
             intent.putExtra("sms_body", "Press see the attached image");
             startActivity(intent);
         });
@@ -57,15 +60,17 @@ public class SMS extends BaseFragment {
             PendingIntent sentPI = PendingIntent.getBroadcast(host.getApplicationContext(), 0, sent, PendingIntent.FLAG_UPDATE_CURRENT);
             Intent delivered = new Intent(SEND_SMS_ACTION);
             PendingIntent deliveredPI = PendingIntent.getBroadcast(host.getApplicationContext(), 0, delivered, PendingIntent.FLAG_UPDATE_CURRENT);
-            manager.sendTextMessage("18888888888", null, "send message via SmsManager", sentPI, deliveredPI);
+            manager.sendTextMessage("88888888888", null, "send message via SmsManager", sentPI, deliveredPI);
             // if the text is too long, use method below
 //            ArrayList<String> messageArray = manager.divideMessage("a long text");
 //            ArrayList<PendingIntent> sentIntents = new ArrayList<>();
 //            for(int i = 0; i < messageArray.size(); i++) {
 //                sentIntents.add(sentPI);
 //            }
-//            manager.sendMultipartTextMessage("18888888888", null, messageArray, sentIntents, null);
+//            manager.sendMultipartTextMessage("88888888888", null, messageArray, sentIntents, null);
         });
+        receiveSMS = new ReceiveSMS();
+        host.registerReceiver(receiveSMS, new IntentFilter(SMS_RECEIVED));
     }
 
     @Override
@@ -82,6 +87,10 @@ public class SMS extends BaseFragment {
 
         if (deliveredReceiver != null) {
             host.unregisterReceiver(deliveredReceiver);
+        }
+
+        if (receiveSMS != null) {
+            host.unregisterReceiver(receiveSMS);
         }
     }
 
@@ -117,6 +126,25 @@ public class SMS extends BaseFragment {
         @Override
         public void onReceive(Context context, Intent intent) {
             show("SMS Delivered");
+        }
+    }
+
+    private class ReceiveSMS extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (SMS_RECEIVED.equals(intent.getAction())) {
+                // receiving messages
+                Object[] pdus = (Object[]) intent.getExtras().get("pdus");
+                if (pdus == null) return;
+                SmsMessage[] messages = new SmsMessage[pdus.length];
+                for (int i = 0; i < pdus.length; i++) {
+                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }
+
+                for (SmsMessage msg : messages) {
+                    show(msg.getMessageBody() + " " + msg.getOriginatingAddress());
+                }
+            }
         }
     }
 }
