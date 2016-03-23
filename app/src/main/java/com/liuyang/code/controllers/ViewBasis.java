9f67797f3316e82e4b0f6536d376064375541436
liuyang <liuyang@xiaolinxiaoli.com>
2016-03-23 12:02:54 +0800

    Scroller example

--- app/src/main/java/com/liuyang/code/controllers/ViewBasis.java
+++ app/src/main/java/com/liuyang/code/controllers/ViewBasis.java
@@ -1,11 +1,14 @@
 package com.liuyang.code.controllers;
 
 import com.liuyang.code.R;
+import com.liuyang.code.widgets.ScrollerView;
 
 /**
  * @author Liuyang 2016/3/22.
  */
 public class ViewBasis extends BaseFragment {
+    private int i = 0;
+
     @Override
     protected int layoutId() {
         return R.layout.view_basis;
@@ -13,7 +16,14 @@ public class ViewBasis extends BaseFragment {
 
     @Override
     protected void init() {
-
+        ScrollerView view = find(R.id.scroller_view);
+        view.setOnClickListener(l -> {
+            if (i++ % 2 == 0) {
+                view.smoothScrollTo(-100, 0);
+            } else {
+                view.smoothScrollTo(0, 0);
+            }
+        });
     }
 
     @Override
