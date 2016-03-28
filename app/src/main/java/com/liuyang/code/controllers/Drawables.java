8a2eae6d6984d9453a7f98cf7baf8a45b692cd1d
liuyang <liuyang@xiaolinxiaoli.com>
2016-03-28 13:28:01 +0800

    bitmap drawable

---
+++ app/src/main/java/com/liuyang/code/controllers/Drawables.java
@@ -0,0 +1,23 @@
+package com.liuyang.code.controllers;
+
+import com.liuyang.code.R;
+
+/**
+ * @author Liuyang 2016/3/28.
+ */
+public class Drawables extends BaseFragment {
+    @Override
+    protected int layoutId() {
+        return R.layout.drawables;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void refresh() {
+
+    }
+}
